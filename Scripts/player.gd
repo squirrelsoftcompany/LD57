a4b765456a60c22146153acc101b76a08e59bf4f
ChristopheClaustre <chris.clst@proton.me>
2025-04-06 15:09:58 +0200

    fix crash in player script

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,8 @@
 extends Node3D
 
-static func get_first_child_of_type(parent : Node, type : String) -> Variant:
-	for child in parent.get_children():
-		if child.is_class(type):
-			return child
-	return null  # Si aucun nœud MeshInstance3D n'est trouvé
 
-
-@onready var _moving_interaction : MovingInteraction = get_first_child_of_type(self, "MovingInteraction")
+@onready var _moving_interaction : MovingInteraction = $MovingInteraction
 
 
 func _ready() -> void:
-	assert(_moving_interaction != null)
 	pass
