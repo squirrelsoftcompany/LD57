61594d4f0a3d68153abf3d3e6926141d064fa46c
AngryCyboot <cyboot25@gmail.com>
2025-04-08 01:37:51 +0200

    MINE-or improvement

--- Scripts/Foes/foes_visu.gd
+++ Scripts/Foes/foes_visu.gd
@@ -1,8 +1,9 @@
 extends MultiMeshInstance3D
 
 
-@export var cube_count = 100
-@export var cluster_radius = 5.0
+@export var cube_count := 100
+@export var cluster_radius := 5.0
+@export var is_mine : bool = false
 
 func generate_instances():
 	var random = RandomNumberGenerator.new()
@@ -12,8 +13,8 @@ func generate_instances():
 		var new_position = Transform3D()
 		new_position = new_position.translated(Vector3(randfn(0.0,0.2) * cluster_radius, randfn(0.0,0.2) * cluster_radius, randfn(0.0,0.2) * cluster_radius))
 		multimesh.set_instance_transform(i, new_position)
-	if get_child_count()>0:
-		$MultiMeshInstance3D2.multimesh = multimesh
+	if is_mine:
+		get_child(0).multimesh = multimesh
 
 func _ready():
 	generate_instances()
