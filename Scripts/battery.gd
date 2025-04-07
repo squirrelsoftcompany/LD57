4b18cde0847775f895707d5ae93662e8812a4fee
Xavier Chalut <xavier.chalut@gmail.com>
2025-04-07 23:56:50 +0200

    Update battery when energy changed

--- Scripts/battery.gd
+++ Scripts/battery.gd
@@ -3,13 +3,13 @@ extends MeshInstance3D
 var max_charge : float = 10
 var charge : float = 0
 
-#TODO : signals
-
 func _ready():
-	max_charge = GlobalPlayerStates.maxEnergy
-	charge = GlobalPlayerStates.current_energy
+	max_charge = (GlobalPlayerStates.maxEnergy *  max_charge ) / GlobalPlayerStates.maxEnergy
+	charge = (GlobalPlayerStates.current_energy *  max_charge ) / GlobalPlayerStates.maxEnergy
 	material_override.set_shader_parameter("charge",charge)
 	material_override.set_shader_parameter("max_charge",max_charge)
+	GlobalEventHolder.connect("energy_state_changed", _energy_change)
 
 func _energy_change():
+	charge = (GlobalPlayerStates.current_energy *  max_charge ) / GlobalPlayerStates.maxEnergy
 	material_override.set_shader_parameter("charge",charge)
