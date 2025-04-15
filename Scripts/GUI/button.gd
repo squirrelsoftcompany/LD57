d194fc372b2f3703f49a0759eb845d61b3f75636
ChristopheClaustre <chris.clst@proton.me>
2025-04-15 23:50:04 +0200

    fix button archive surfing buttons key text that isn't supported on webbrowser

--- Scripts/GUI/button.gd
+++ Scripts/GUI/button.gd
@@ -5,10 +5,11 @@ class_name SpecialButton
 
 @export var key:="A" : set = set_key
 @export_multiline var help:="do\nsomething" : set = set_help
+@export var icon: Texture2D = null : set = set_icon
 @export var shortcut : Shortcut = null : set = set_shortcut
 @export var ask_signal_name : String = ""
-@export var disabled : bool = false : set = set_disabled
-@export var button_pressed : bool = false : set = set_button_pressed, get = get_button_pressed
+@export var disabled : bool = false : get = get_disabled, set = set_disabled
+@export var button_pressed : bool = false : get = get_button_pressed, set = set_button_pressed
 @export_enum("NORMAL:0", "HOLD:1", "TOGGLE:2") var mode : int = 0 : set = set_mode
 
 
@@ -16,33 +17,29 @@ func set_key(value):
 	key = value
 	$"Button/Label".text = key
 
-
 func set_help(value):
 	help = value
 	$Label.text = help
 
+func set_icon(value):
+	icon = value
+	if is_instance_valid(get_node_or_null("Button")):
+		$Button.icon = icon
 
 func set_shortcut(value):
 	shortcut = value
 	$Button.shortcut = shortcut
 
+func get_disabled(): return $Button.disabled
+func set_disabled(value): $Button.disabled = value
 
-func set_disabled(value):
-	disabled = value
-	$Button.disabled = disabled
+func get_button_pressed(): return $Button.button_pressed
+func set_button_pressed(value): $Button.button_pressed = value
 
 
 func set_mode(value):
 	mode = value
 	$Button.toggle_mode = mode > 0
-
-
-func set_button_pressed(value):
-	$Button.button_pressed = value
-
-
-func get_button_pressed():
-	return $Button.button_pressed
 
 
 func _ready() -> void:
