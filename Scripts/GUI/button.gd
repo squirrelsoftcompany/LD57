82fd59a65bbdb273a178b3c2610632320ad70152
ChristopheClaustre <chris.clst@proton.me>
2025-04-21 23:37:36 +0200

    SpecialButton now set their key text thanks to the InputEventAction

--- Scripts/GUI/button.gd
+++ Scripts/GUI/button.gd
@@ -3,7 +3,8 @@ extends VBoxContainer
 class_name SpecialButton
 
 
-@export var key:="A" : set = set_key
+@export var force_key_text := "" : set = set_force_key_text
+#@export var key:="A" : set = set_key
 @export_multiline var help:="do\nsomething" : set = set_help
 @export var icon: Texture2D = null : set = set_icon
 @export var shortcut : Shortcut = null : set = set_shortcut
@@ -13,9 +14,9 @@ class_name SpecialButton
 @export_enum("NORMAL:0", "HOLD:1", "TOGGLE:2") var mode : int = 0 : set = set_mode
 
 
-func set_key(value):
-	key = value
-	$"Button/Label".text = key
+func set_force_key_text(value):
+	force_key_text = value
+	compute_key_text()
 
 func set_help(value):
 	help = value
@@ -29,6 +30,7 @@ func set_icon(value):
 func set_shortcut(value):
 	shortcut = value
 	$Button.shortcut = shortcut
+	compute_key_text()
 
 func get_disabled(): return $Button.disabled
 func set_disabled(value): $Button.disabled = value
@@ -61,3 +63,27 @@ func _on_button_toggled(toggled_on: bool) -> void:
 		var ask_signal_name_full := ask_signal_name + ("_start" if toggled_on else "_finish")
 		print(name, " toggled(", toggled_on, ") -> ", ask_signal_name_full)
 		GlobalEventHolder.emit_signal(ask_signal_name_full)
+
+
+func compute_key_text():
+	if !get_node_or_null("Button/Label"):
+		return
+	if force_key_text:
+		$Button/Label.text = force_key_text
+		return
+	if shortcut == null or shortcut.events.size() < 0:
+		$Button/Label.text = "ERR"
+		return
+	var action_name := shortcut.events[0].action as String
+	var inputEvents := InputMap.action_get_events(action_name)
+	if !inputEvents.is_empty():
+		if inputEvents[0].keycode:
+			$Button/Label.text = OS.get_keycode_string(inputEvents[0].keycode).substr(0,3)
+		elif inputEvents[0].physical_keycode:
+			var keycode : Key = inputEvents[0].physical_keycode
+			keycode = DisplayServer.keyboard_get_keycode_from_physical(keycode)
+			$Button/Label.text = OS.get_keycode_string(keycode).substr(0,3)
+		else:
+			$Button/Label.text = "ERR"
+	else:
+		$Button/Label.text = "ERR"
